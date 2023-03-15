6fd088cb925b944e2efca8d52ffeefec512147d1
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-03-15 18:43:03 +0300

    assignment commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+const currentUser = 'Anthony Mulinge';
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//const welcomeMessage = currentUser;
+
+//const welcomeMessage = 'Welcome to Flatbook, currentUser';
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = '';
+
+//const shortGreeting = 'Welcome, ';
+
+//const shortGreeting = `Welcome, ${currentUser}`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
